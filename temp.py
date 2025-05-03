ebdc59b3a6f0d514f00f8c4178c1953fc7dda217
Anurag Kumar Jha <anurag.jha.in@gmail.com>
2025-05-02 23:44:47 -0700

    Fix script

--- temp.py
+++ temp.py
@@ -3,7 +3,7 @@ import pathlib
 from typing import List, Set
 
 # Files, folders and extensions to ignore
-ignored_files = {"yarn.lock", "temp.py", "temp.txt", "README.md"}
+ignored_files = {"yarn.lock", "temp.py", "temp.txt", "README.md", "Developer-Manual.md"}
 ignored_folders = {"node_modules", ".vscode", ".git"}
 ignored_extensions = {".png", ".wav", ".ico", ".svg", ".webp"}
 
