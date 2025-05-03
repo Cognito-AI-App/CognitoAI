80239f082f6f0886be0f8fbc255dbdda6f44e090
Anurag Kumar Jha <anurag.jha.in@gmail.com>
2025-05-02 23:43:48 -0700

    Stable Base

--- src/app/(client)/dashboard/page.tsx
+++ src/app/(client)/dashboard/page.tsx
@@ -160,7 +160,7 @@ function Interviews() {
                     </div>
                     <p className="text-l text-center">
                       Contact{" "}
-                      <span className="font-semibold">founders@folo-up.co</span>{" "}
+                      <span className="font-semibold">anurag.jha.in@gmail.com</span>{" "}
                       to upgrade your plan.
                     </p>
                   </div>
